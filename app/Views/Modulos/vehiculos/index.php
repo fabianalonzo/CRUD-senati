cbff4a2e7728164e4bd5e8107aa74032a7916da6
Fabian Salas <fabianalonzo@gmail.com>
2026-03-27 11:24:57 -0500

    UI Vehiculos

--- app/Views/Modulos/vehiculos/index.php
+++ app/Views/Modulos/vehiculos/index.php
@@ -1,10 +1,81 @@
 <?= $header ?>
 
 <div class="row">
-  <div class="col-md-12">
-    <h5>Administrador de vehiculos</h5> 
-    
-  </div>
+    <div class="col-md-12">
+        <h5>Administrador de vehículos</h5>
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-vehiculos">
+            Nuevo vehículo
+        </button>
+        <table class="table table-sm mt-3">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Marca</th>
+                    <th>Modelo</th>
+                    <th>Año</th>
+                    <th>Color</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>
 </div>
 
+<!-- Zona de modal -->
+
+<div class="modal fade" id="modal-vehiculos" data-backdrop="static" data-keyboard="false" tabindex="-1"
+    aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header bg-primary text-white">
+                <h5 class="modal-title" id="exampleModalLabel">Complete el formulario</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="" id="formulario-vehiculos" autocapitalize="off">
+                    <div class="form-group">
+                        <label for="marcas">Marca:</label>
+                        <select name="marcas" id="marcas" class="form-control rounded-0" required>
+                            <option value="">Seleccione </option>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="modelo">Modelo:</label>
+                        <input type="text" name="modelo" id="modelo" class="rounded-0 form-control" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="anio">Año:</label>
+                        <input type="text" name="anio" id="anio" class="rounded-0 form-control" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="color">Color:</label>
+                        <input type="text" name="color" id="color" class="rounded-0 form-control" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="precio">Precio:</label>
+                        <input type="text" name="precio" id="precio" class="rounded-0 form-control text-right" required>
+                    </div>
+
+
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-sm btn-outline-danger" data-dismiss="modal">Cancelar</button>
+                <button type="submit" form="formulario-vehiculos" class="btn btn-sm btn-outline-primary">Guardar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Fin zona de modal -->
 <?= $footer ?>

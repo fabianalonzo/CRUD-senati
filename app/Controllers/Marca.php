a0f17c016e9732c04de521b84010c8992c28d834
Fabian Salas <fabianalonzo@gmail.com>
2026-03-27 12:43:28 -0500

    Controlasor para marcas

---
+++ app/Controllers/Marca.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Controllers;
+use App\Controllers\BaseController;
+use App\Models\MarcaModel;
+
+class Marca extends BaseController{
+  public function getMarcas(){
+    $marca = new MarcaModel();
+    return $this->response->setJSON($marca->findAll());
+  }
+}
